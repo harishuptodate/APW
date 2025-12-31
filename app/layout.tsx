65e5d79a908e73238d2edc68f23d41ace9830d38
harishuptodate <159016151+harishuptodate@users.noreply.github.com>
2025-12-31 23:07:29 +0530

    Update metadata for RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'AmazonProductImageAPI',
+  description: 'Use this API to fetch product images from Amazon URLs programmatically.',
+  generator: '',
 }
 
 export default function RootLayout({
